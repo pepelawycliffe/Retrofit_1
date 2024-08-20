3a0bbd3a6428f93cbae245bf8916d70589e0a0f6
wycliffe <wycliffepepela01@gmail.com>
2024-08-20 13:35:00 +0300

    Retrofit and recyclerview in jetpack compose

--- app/src/main/java/com/example/retrofit_recycler_view/view/HomeUi.kt
+++ app/src/main/java/com/example/retrofit_recycler_view/view/HomeUi.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.CrossAxisAlignment.CenterCrossAxisAlignment.align
 //import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,9 +21,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -35,58 +39,81 @@ import com.example.retrofit_recycler_view.model.Movie
 fun MovieItem(movie: Movie){
 
     Card (
-//        elevation = 10.dp,
         border = BorderStroke(1.dp, Color.Blue),
         modifier = Modifier
-        .padding(8.dp, 4.dp)
-        .fillMaxWidth()
-        .height(110.dp),
+            .padding(12.dp, 8.dp, 8.dp, 12.dp)
+            .fillMaxWidth()
+            .height(110.dp),
         shape = RoundedCornerShape(8.dp)
 
         ){
         Surface {
             Row (modifier = Modifier
-                .padding(4.dp)
-                .fillMaxSize()) {
-                Image(painter = rememberImagePainter(data = movie.imageUrl,
-                    builder ={
-                        scale(Scale.FILL)
-                        transformations(CircleCropTransformation())
-                    }), contentDescription =movie.desc
-                    , modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(0.2f)
-                )
-                Column (
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .padding(4.dp)
-                        .weight(0.8f))
-                {
-                    Text(text = movie.name,
-                        style = MaterialTheme.typography.labelSmall,
-                        modifier = Modifier
-                            .padding(4.dp)
-                            .background(
-                                Color.LightGray
-                            )
+                .padding(8.dp)
+                .fillMaxSize())
+            {
+                Card (modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(0.2f)
+                    .padding(0.dp,0.dp,4.dp)
+                    .weight(0.2f)
+
+                    ){
+                    Image(painter = rememberImagePainter(data = movie.imageUrl,
+                        builder ={
+                            scale(Scale.FILL)
+                            transformations()
+//                            transformations(CircleCropTransformation())
+                        }), contentDescription =movie.desc
+                        ,modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(0.2f),
+                        alignment =Alignment.Center,
+//                        alpha = 0.2f,
                     )
-                    Text(
-                        text = movie.category,
-                        style = MaterialTheme.typography.titleSmall,
-                        modifier = Modifier
-                            .padding(4.dp)
-
-                    )
-                    Text(
-                        text = movie.desc,
-                        style = MaterialTheme.typography.displaySmall,
-                        modifier = Modifier, maxLines = 2
-
-
-                    )
-
                 }
+
+               Card(
+                   modifier = Modifier
+                       .background(color = Color.LightGray)
+                       .fillMaxSize()
+                       .weight(0.8f)
+                   ,shape = RoundedCornerShape(4.dp)
+                   ,border = BorderStroke(1.dp, Color.Gray),) {
+                   Column (
+                       verticalArrangement = Arrangement.Center,
+                       modifier = Modifier
+                           .padding(4.dp)
+//                           .weight(0.8f)
+                   )
+                   {
+                       Text(text = movie.name,
+                           style = MaterialTheme.typography.titleMedium,
+                           modifier = Modifier
+                               .padding(4.dp)
+                               .fillMaxWidth()
+//                            .background(Color.LightGray)
+                           ,
+                           fontWeight = FontWeight.Normal,
+                           overflow = TextOverflow.Ellipsis,
+                       )
+                       Text(
+                           text = movie.category,
+                           style = MaterialTheme.typography.titleMedium,
+                           modifier = Modifier
+                               .padding(4.dp)
+
+                       )
+                       Text(
+                           text = movie.desc,
+                           style = MaterialTheme.typography.titleSmall,
+                           modifier = Modifier, maxLines = 2
+
+
+                       )
+
+                   }
+               }
 
             }
         }
